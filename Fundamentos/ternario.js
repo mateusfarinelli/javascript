ef39f4728ae9a5d9e9944c9071dc2db50c8ad1e1
Mateus Farinelli <mateusfarinellizardo@gmail.com>
2019-12-27 08:45:13 -0300

    Fundamentos

--- Fundamentos/ternario.js
+++ Fundamentos/ternario.js
@@ -1,7 +1,9 @@
 /*
   Operador ternario trabalha com 3 operandos, sendo:
   Operador 1 -> Uma expressão relacional
-  Opeador 2 e Operador 3 -> retornos da expressão
+  Opeador 2 e Operador 3 -> retornos da expressão separados por ":" onde o operando
+  a esquerda do operador é o retorno da condição caso seja verdadeiro e outro é o retorno
+  caso a expressão seja falsa
 
   O operador ternario é presentado por "?" e os retornos sao dados pelo oprador ":"
 */
